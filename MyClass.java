57884125a30f421e0c5ad341451001df54f73b4e
stalkerpnv <stalker_pnv@mail.ru>
2023-02-06 14:56:07 +0900

    Add .gitignore

--- MyClass.java
+++ MyClass.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
 public class MyClass{
   public static void main(String[]args){
-    System.out.println("Hello World");
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Input your name");
+    String name = sc.nextLine();
+    System.out.println("Hello " + name);
   }
 }
